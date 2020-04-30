67b87d2768487123641977548ca6cbc04d4bb08e
Dylan Hudson <dylanhudson1998@gmail.com>
2020-04-30 19:48:15 +0100

    Update MonitoringStation.java

--- src/MonitoringStation.java
+++ src/MonitoringStation.java
@@ -1,5 +1,6 @@
 import AssignmentTwo.*;
 import AssignmentTwo.LocalServer;
+
 import org.omg.CORBA.ORB;
 import org.omg.CosNaming.NameComponent;
 import org.omg.CosNaming.NamingContextExtHelper;
@@ -7,8 +8,6 @@ import org.omg.PortableServer.POA;
 import org.omg.PortableServer.POAHelper;
 
 import javax.swing.*;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -16,108 +15,9 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Properties;
 
-class MonitoringStationServant extends MonitoringStationPOA
-{
-    //Args
-    public StationDetails stationDetails;
-    private MonitoringStation parent;
-    private ArrayList<Reading> readings;
-
-    private Timer timer;
-    private static final int readingAlert= 25;
-
-    public MonitoringStationServant(MonitoringStation parentGUI)
-    {
-        parent = parentGUI;
-        readings = new ArrayList<>();
-    }
-
-    @Override
-    public Reading reading()
-    {
-        //Get Reading
-        int readingValue = parent.getReadingValue();
-        String stationName = get_details().station_name;
-        int time = timeAsInt();
-        int date = Calendar.getInstance().get(Calendar.DAY_OF_YEAR);
-
-        //Create and check value of reading
-        Reading reading = new Reading(time, date, stationName, readingValue);
-        if(readingValue >= readingAlert)
-        {
-            parent.localServant.send_alert(reading);
-        }
-        parent.populateLog();
-        return reading;
-    }
-
-    @Override
-    public Reading[] reading_log()
-    {
-        return readings.toArray(new Reading[0]);
-    }
-
-    @Override
-    public void get_reading()
-    {
-        readings.add(reading());
-    }
-
-    @Override
-    public void set_details(StationDetails info)
-    {
-        this.stationDetails = info;
-    }
-
-    @Override
-    public StationDetails get_details()
-    {
-        return stationDetails;
-    }
-
-    @Override
-    public void activate()
-    {
-        timer = new Timer(10000, new ActionListener()
-        {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent)
-            {
-                get_reading();
-                parent.populateLog();
-            }
-        });
-        timer.start();
-    }
-
-    @Override
-    public void deactivate()
-    {
-        timer.stop();
-    }
-
-    @Override
-    public void reset()
-    {
-        parent.clearLog();
-        readings.clear();
-    }
-
-    public int timeAsInt()
-    {
-        int hours = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
-        int minute = Calendar.getInstance().get(Calendar.MINUTE);
-        return hours * 60 + minute;
-    }
-}
 
 public class MonitoringStation extends JFrame
 {
-    //Args
-    private String name;
-    private String location;
-    private String localServer;
-
     //Initialise GUI elements
     JPanel panel;
     JScrollPane scrollPane;
@@ -132,18 +32,17 @@ public class MonitoringStation extends JFrame
     MonitoringStationServant servant = new MonitoringStationServant(this);
     LocalServer localServant;
 
-    //Servant setup
     public MonitoringStation(String[] args)
     {
-        //Check input and start servant
         if(args.length > 3)
         {
             System.out.println("You have too many arguments");
             return;
         }
-        name = args[0];
-        location = args[1];
-        localServer = args[2];
+
+        String name = args[0];
+        String location = args[1];
+        String localServer = args[2];
 
         if(name == null || location == null || localServer == null)
         {
@@ -288,11 +187,6 @@ public class MonitoringStation extends JFrame
         });
     }
 
-    public int getReadingValue()
-    {
-        return readingSlider.getValue();
-    }
-
     public void populateLog()
     {
         clearLog();
@@ -306,6 +200,12 @@ public class MonitoringStation extends JFrame
     {
         textarea.setText("");
     }
+
+    public int getReadingValue()
+    {
+        return readingSlider.getValue();
+    }
+
     public static void main(String[] args)
     {
         //Build our initialisation screen
@@ -338,3 +238,96 @@ public class MonitoringStation extends JFrame
         }
     }
 }
+
+class MonitoringStationServant extends MonitoringStationPOA
+{
+    public StationDetails stationDetails;
+    private MonitoringStation parent;
+    private ArrayList<Reading> readings;
+
+    private Timer timer;
+    private static final int readingAlert= 25;
+
+    public MonitoringStationServant(MonitoringStation parentGUI)
+    {
+        parent = parentGUI;
+        readings = new ArrayList<>();
+    }
+
+    @Override
+    public Reading reading()
+    {
+        int readingValue = parent.getReadingValue();
+        String stationName = get_details().station_name;
+        int time = timeAsInt();
+        int date = Calendar.getInstance().get(Calendar.DAY_OF_YEAR);
+
+        Reading reading = new Reading(time, date, stationName, readingValue);
+        if(readingValue >= readingAlert)
+        {
+            parent.localServant.send_alert(reading);
+        }
+        parent.populateLog();
+        return reading;
+    }
+
+    @Override
+    public Reading[] reading_log()
+    {
+        return readings.toArray(new Reading[0]);
+    }
+
+    @Override
+    public void get_reading()
+    {
+        readings.add(reading());
+    }
+
+    @Override
+    public StationDetails get_details()
+    {
+        return stationDetails;
+    }
+
+    @Override
+    public void set_details(StationDetails info)
+    {
+        this.stationDetails = info;
+    }
+
+    @Override
+    public void activate()
+    {
+        timer = new Timer(10000, new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent)
+            {
+                get_reading();
+                parent.populateLog();
+            }
+        });
+        timer.start();
+    }
+
+    @Override
+    public void deactivate()
+    {
+        timer.stop();
+    }
+
+    @Override
+    public void reset()
+    {
+        parent.clearLog();
+        readings.clear();
+    }
+
+    public int timeAsInt()
+    {
+        int hours = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+        int minute = Calendar.getInstance().get(Calendar.MINUTE);
+        return hours * 60 + minute;
+    }
+}
+
