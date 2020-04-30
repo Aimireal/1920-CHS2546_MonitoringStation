9c8f63a31329dd1414521a07219be8ddb46a6893
Dylan Hudson <dylanhudson1998@gmail.com>
2020-04-30 19:47:35 +0100

    Update LocalServer.java

--- src/LocalServer.java
+++ src/LocalServer.java
@@ -1,6 +1,7 @@
 import AssignmentTwo.*;
 import AssignmentTwo.MonitoringCentre;
 import AssignmentTwo.MonitoringStation;
+
 import org.omg.CORBA.ORB;
 import org.omg.CosNaming.NameComponent;
 import org.omg.CosNaming.NamingContextExt;
@@ -16,179 +17,8 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 
-class LocalServerServant extends LocalServerPOA
-{
-    //ArrayLists to hold our stations, readings and alerts
-    private ArrayList<StationDetails> connectedStations;
-    private ArrayList<Reading> readings;
-    private ArrayList<Reading> alerts;
-
-    //Args
-    private ServerDetails serverDetails;
-    private LocalServer parent;
-    private ORB orb;
-    private NamingContextExt namingService;
-    private int timePeriod = 1;
-
-    public LocalServerServant(ORB orbValue, LocalServer parent)
-    {
-        this.parent = parent;
-        connectedStations = new ArrayList<>();
-        readings = new ArrayList<>();
-        alerts = new ArrayList<>();
-
-        try
-        {
-            orb = orbValue;
-            org.omg.CORBA.Object namingServiceObj = orb.resolve_initial_references("NameService");
-            if(namingServiceObj == null)
-                return;
-
-            namingService = NamingContextExtHelper.narrow(namingServiceObj);
-        } catch(Exception e)
-        {
-            System.err.println("Error: " + e);
-            e.printStackTrace(System.out);
-        }
-    }
-
-    @Override
-    public StationDetails[] connected_stations()
-    {
-        return connectedStations.toArray(new StationDetails[0]);
-    }
-
-    @Override
-    public ServerDetails get_server_details()
-    {
-        return serverDetails;
-    }
-
-    @Override
-    public void set_details(ServerDetails serverDetails)
-    {
-        this.serverDetails = serverDetails;
-    }
-
-    @Override
-    public Reading[] all_readings()
-    {
-        for(StationDetails server : connectedStations)
-        {
-            String stationName = server.station_name;
-            try
-            {
-                //Station reference
-                MonitoringStation monitoringStation = MonitoringStationHelper.narrow(namingService.resolve_str(stationName));
-                ArrayList<Reading> allReadings = new ArrayList<>(Arrays.asList(monitoringStation.reading_log()));
-
-                //Check if we have any new readings
-                //ToDo: We might only need to check Date/Time and station name. Probably can do this in line?
-                if(allReadings.size() != 0)
-                {
-                    for (Reading pulledReading : allReadings)
-                    {
-                        if (!readingReader(readings, pulledReading))
-                        {
-                            readings.add(pulledReading);
-                        }
-                    }
-                }
-            } catch(Exception e)
-            {
-                e.printStackTrace();
-            }
-        }
-        return readings.toArray(new Reading[0]);
-    }
-
-    @Override
-    public Reading[] get_readings(String station_name)
-    {
-        try
-        {
-            MonitoringStation monitoringStation = MonitoringStationHelper.narrow(namingService.resolve_str(station_name));
-            return monitoringStation.reading_log();
-        } catch(Exception e)
-        {
-            return new Reading[0];
-        }
-    }
-
-    @Override
-    public Reading[] get_current_readings()
-    {
-        ArrayList<Reading> readings = new ArrayList<>();
-        for(StationDetails s: connectedStations)
-        {
-            try
-            {
-                MonitoringStation monitoringStation = MonitoringStationHelper.narrow(namingService.resolve_str(s.station_name));
-                readings.add(monitoringStation.reading());
-            } catch(NotFound | CannotProceed | InvalidName e)
-            {
-                e.printStackTrace();
-                return new Reading[0];
-            }
-        }
-        return readings.toArray(new Reading[0]);
-    }
-
-    @Override
-    public Reading[] alerts()
-    {
-        return alerts.toArray(new Reading[0]);
-    }
-
-    @Override
-    public void register_monitoring_station(StationDetails info)
-    {
-        connectedStations.add(info);
-        System.out.println("New Station: " + info.station_name);
-    }
-
-
-    @Override
-    public void send_alert(Reading reading)
-    {
-        ArrayList<Reading> alertsSend = alerts.stream().filter(
-                r->!r.station_name.equals(reading.station_name)
-                        && r.date == reading.date
-                        && (r.time + timePeriod >= reading.time || r.time == reading.time))
-                .collect(Collectors.toCollection(ArrayList::new));
-
-        if(!readingReader(alerts, reading))
-        {
-            alerts.add(reading);
-            alertsSend.add(reading);
-        }
-
-        if(alertsSend.size() > 1)
-        {
-            Alert alert = new Alert(this.get_server_details().server_name, alertsSend.toArray(new Reading[0]));
-            parent.centreServant.send_alert(alert);
-        }
-    }
-
-    public boolean readingReader(ArrayList<Reading> list, Reading r)
-    {
-        for(Reading reading: list)
-        {
-            if((reading.reading_level == r.reading_level)
-                    && (reading.station_name.equals(r.station_name))
-                    && (reading.date == r.date)
-                    && (reading.time == r.time))
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-}
-
 public class LocalServer
 {
-    //Args
     private String name;
     private String location;
     private String monitoringCentre;
@@ -251,7 +81,7 @@ public class LocalServer
 
     public static void main(String[] args)
     {
-        //Nasty way to initialise our instance as I can't refer to our non static current values
+        //Initialise screen
         JFrame frame = new JFrame();
         String namePrompt = "Please enter the servers name";
         String name = JOptionPane.showInputDialog(frame, namePrompt);
@@ -271,5 +101,168 @@ public class LocalServer
         {
             JOptionPane.showMessageDialog(frame, "Please provide all details", "Error", JOptionPane.WARNING_MESSAGE);
         }
+    }
+}
+
+class LocalServerServant extends LocalServerPOA
+{
+    private ArrayList<StationDetails> connectedStations;
+    private ArrayList<Reading> readings;
+    private ArrayList<Reading> alerts;
+
+    private ServerDetails serverDetails;
+    private LocalServer parent;
+    private NamingContextExt namingService;
+    private int timePeriod = 1;
+
+    public LocalServerServant(ORB orbValue, LocalServer parent)
+    {
+        this.parent = parent;
+        connectedStations = new ArrayList<>();
+        readings = new ArrayList<>();
+        alerts = new ArrayList<>();
+
+        try
+        {
+            org.omg.CORBA.Object namingServiceObj = orbValue.resolve_initial_references("NameService");
+            if(namingServiceObj == null)
+                return;
+
+            namingService = NamingContextExtHelper.narrow(namingServiceObj);
+        } catch(Exception e)
+        {
+            System.err.println("Error: " + e);
+            e.printStackTrace(System.out);
+        }
+    }
+
+    @Override
+    public StationDetails[] connected_stations()
+    {
+        return connectedStations.toArray(new StationDetails[0]);
+    }
+
+    @Override
+    public ServerDetails get_server_details()
+    {
+        return serverDetails;
+    }
+
+    @Override
+    public void set_details(ServerDetails serverDetails)
+    {
+        this.serverDetails = serverDetails;
+    }
+
+    @Override
+    public void register_monitoring_station(StationDetails info)
+    {
+        connectedStations.add(info);
+        System.out.println("New Station: " + info.station_name);
+    }
+
+    @Override
+    public Reading[] all_readings()
+    {
+        for(StationDetails server : connectedStations)
+        {
+            String stationName = server.station_name;
+            try
+            {
+                MonitoringStation monitoringStation = MonitoringStationHelper.narrow(namingService.resolve_str(stationName));
+                ArrayList<Reading> allReadings = new ArrayList<>(Arrays.asList(monitoringStation.reading_log()));
+
+                //Check if we have any new readings
+                if(allReadings.size() != 0)
+                {
+                    for (Reading pulledReading : allReadings)
+                    {
+                        if (!readingReader(readings, pulledReading))
+                        {
+                            readings.add(pulledReading);
+                        }
+                    }
+                }
+            } catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+        return readings.toArray(new Reading[0]);
+    }
+
+    @Override
+    public Reading[] get_readings(String station_name)
+    {
+        try
+        {
+            MonitoringStation monitoringStation = MonitoringStationHelper.narrow(namingService.resolve_str(station_name));
+            return monitoringStation.reading_log();
+        } catch(Exception e)
+        {
+            return new Reading[0];
+        }
+    }
+
+    @Override
+    public Reading[] get_current_readings()
+    {
+        ArrayList<Reading> readings = new ArrayList<>();
+        for(StationDetails s: connectedStations)
+        {
+            try
+            {
+                MonitoringStation monitoringStation = MonitoringStationHelper.narrow(namingService.resolve_str(s.station_name));
+                readings.add(monitoringStation.reading());
+            } catch(NotFound | CannotProceed | InvalidName e)
+            {
+                e.printStackTrace();
+                return new Reading[0];
+            }
+        }
+        return readings.toArray(new Reading[0]);
+    }
+
+    @Override
+    public Reading[] alerts()
+    {
+        return alerts.toArray(new Reading[0]);
+    }
+
+    @Override
+    public void send_alert(Reading reading)
+    {
+        ArrayList<Reading> alertsSend = alerts.stream().filter(
+                r->!r.station_name.equals(reading.station_name)
+                        && r.date == reading.date
+                        && (r.time + timePeriod >= reading.time || r.time == reading.time))
+                .collect(Collectors.toCollection(ArrayList::new));
+
+        if(!readingReader(alerts, reading))
+        {
+            alerts.add(reading);
+            alertsSend.add(reading);
+        }
+
+        if(alertsSend.size() > 1)
+        {
+            Alert alert = new Alert(this.get_server_details().server_name, alertsSend.toArray(new Reading[0]));
+            parent.centreServant.send_alert(alert);
+        }
+    }
+
+    public boolean readingReader(ArrayList<Reading> list, Reading r)
+    {
+        for(Reading reading: list)
+        {
+            if((reading.reading_level == r.reading_level)
+                    && (reading.station_name.equals(r.station_name))
+                    && (reading.date == r.date)
+                    && (reading.time == r.time))
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
